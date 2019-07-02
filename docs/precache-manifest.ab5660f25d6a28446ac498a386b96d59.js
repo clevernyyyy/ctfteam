3d73ebaf3931c1311bf7d8ff6fc5248a634a5e6f
Adam Schaal <adam.schaal@contrastsecurity.com>
2019-07-02 00:27:13 -0500

    adding create-react-app template

---
+++ docs/precache-manifest.ab5660f25d6a28446ac498a386b96d59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fccb8847eec3a3428d97e4a54c92283",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa2e9efae59076182e67",
+    "url": "/static/css/main.cee8faa2.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "aa2e9efae59076182e67",
+    "url": "/static/js/main.063dfc3c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
